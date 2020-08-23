359bf3d89e054e58c54ecbfddab694fbb118367c
oryay <orya.yeho@gmail.com>
2020-08-23 21:42:02 +0300

    keyEncoding()

--- encryption/encryption.c
+++ encryption/encryption.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 void exitError(char** messege)
 {
 	printf(messege);
-	printf("/n");
+	printf("\n");
 	getchar();
 	exit(1);
 }
@@ -33,19 +33,46 @@ void encryptingFile(FILE* file, char** key)
 	printf("\n");
 }
 
-int* keyEncoding(char** key_string)
+int* keyEncoding(char** key_string, int** key_numbers)
 {
-	if (strlen(key_string) < 8) {
+	// Input: key_string - String of key.
+	// Output: Array of number. The numbers is values ​​of the letters (A = 1).
+	int lengthKey = strlen(key_string);
+	int i;
+	char ch;
+
+	if (lengthKey < 8) {
 		exitError("Key less than 8 characters.");
 	}
+
+	*key_numbers = malloc(lengthKey * sizeof(int));
+
+	for (i = 0; i < lengthKey; i++)
+	{
+		ch = *key_string + i;
+		*(*key_numbers + i) = ch - 'A' + 1;
+	}
+	
+	return lengthKey;
 }
 
 
 void main() {
 	FILE *f_in = openFileToEncryption("try.txt");
+	char** key_string = "ABCDEFGH";
+	int* key_numbers;
+	int lengthKey,i;
 
-	encryptingFile(f_in, "key");
-	printf("Amazing");
+
+	encryptingFile(f_in, key_string);
+	lengthKey = keyEncoding(key_string, &key_numbers);
+
+	for (i = 0; i < lengthKey; i++) {
+		printf("%d ", key_numbers[i]);
+	}
+
+	printf("\nAmazing");
+	free(key_numbers);
 
 	
 }
