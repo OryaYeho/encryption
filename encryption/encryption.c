988bbac4b7e9b97e726b01d93ee4cba3e672e16e
oryay <orya.yeho@gmail.com>
2020-08-23 17:53:24 +0300

    openFileToEncription() - from try.txt

--- encryption/encryption.c
+++ encryption/encryption.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void openFileToEncryption()
+{
+	FILE* f_in;
+	fopen_s(&f_in, "try.txt", "r+");
+	if (f_in == NULL)
+	{
+		printf("File not found\n");
+	}
+}
+
+
 void main() {
+	openFileToEncryption();
+
 	printf("test");
 }
