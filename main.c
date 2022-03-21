3af58704068b8eb7dc55dea7ed7e5d09fa595388
oryay <orya.yeho@gmail.com>
2022-03-21 12:14:52 +0200

    function crypt()

--- main.c
+++ main.c
@@ -7,15 +7,38 @@
 
 #define TEMP_NAME "tempFile"
 
-void main(int argc, char* argv[]) {
+void crypt(void (*action_fun)(FILE*, FILE*,char*), char* key, char* message) {
 	FILE* fd_in;
-	FILE* fd_out;
+	FILE* fd_out = creatingFile(TEMP_NAME);
+	char filename[20];
+
+	printf("\nEnter file name: ");
+	scanf_s("%s", filename, 20);
+
+	fd_in = openFile(filename);
+
+	action_fun(fd_in, fd_out, key);
+	puts(message);
+
+	if (deleteOriginalFile(fd_in, filename) != 0)
+	{
+		printf("Unable to delete the original file.\n");
+		printf("The encrypted file is named '");
+		printf(TEMP_NAME);
+		printf("'.\n");
+	}
+	else
+	{
+		renameEncryptedFile(fd_out, TEMP_NAME, filename);
+	}
+}
+
+void main(int argc, char* argv[]) {
 
 	char action;
-
-	char filename[20];
 	char* key = "orya";
-	int actionFlag;
+
+
 
 	//logIn();
 
@@ -24,23 +47,24 @@ void main(int argc, char* argv[]) {
 	printf(" 2 - To do decryption to file.\n");
 	action = getchar();
 
-	printf("\nEnter file name: ");
-	//scanf_s("%s", filename);
-	scanf_s("%s", filename,20);
+	
 
 	switch (action)
 	{
 	case '1':
-		fd_in = openFile(filename);
+		/*fd_in = openFile(filename);
 		fd_out = creatingFile(TEMP_NAME);
 		encryptingFile(fd_in, fd_out, key);
-		printf("Encryption has been performed.\n");
+		printf("Encryption has been performed.\n");*/
+		//message = "Encryption has been performed.";
+		crypt(encryptingFile, key, "Encryption has been performed.");
 		break;
 	case '2':
-		fd_in = openFile(filename);
+		/*fd_in = openFile(filename);
 		fd_out = creatingFile(TEMP_NAME);
 		decryptingFile(fd_in, fd_out, key);
-		printf("The decoding was performed.\n");
+		printf("The decoding was performed.\n");*/
+		crypt(decryptingFile, key, "The decoding was performed.");
 		break;
 	default:
 		exitError("Wrong action!");
